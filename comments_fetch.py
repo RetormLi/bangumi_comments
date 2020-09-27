114fe6c004eb59e7279ec1932c360c0fb7e3ecbd
RetormLi <1197334522@qq.com>
2020-09-27 20:00:27 +0800

    Fix some bugs.

--- comments_fetch.py
+++ comments_fetch.py
@@ -21,73 +21,95 @@ user_agent_list = [
 ]
 page_re = re.compile(r"<a href=\"/anime/list/.*?/collect\?page=([0-9]+)\" class=\"p\">[0-9]+</a>", re.UNICODE)
 item_re = re.compile(
-    r"<a href=\"/subject/[0-9]+\" class=\"l\">(.*?)</a>.*?<span class=\"starlight stars([0-9]+)\">.*?style=\".*?\"><div class=\"text\"> (.*?)<",
+    r"<li id=\"item_[0-9]+\" class=\".*?\">.*?class=\"l\">(.*?)</a>(.*?<span class=\"starlight stars([0-9]+)\">)?.*?(<div class=\"text\"> (.*?))?</div>.*?</li>",
     re.UNICODE)
 url = r"https://bangumi.tv/anime/list/" + user_id + "/collect"
 date_time = str(datetime.now().date())
 time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-save_path = 'comments_' + user_id + '_' + date_time + '.json'
+save_path = 'log/comments_' + user_id + '.json'
 
 
 def get_item(pair, d):
-    name, star, comment = pair
+    name, _, star, _, comment = pair
     if name not in d:
-        d[name] = {'star': star, 'comment': comment, 'time': time}
+        d[name] = dict()
+        if star != '':
+            d[name]['star'] = star
+        if comment != '':
+            d[name]['comment'] = comment
+        d[name]['time'] = time
         return
-    if d[name]['star'] != star:
+
+    if star != '' and 'star' not in d[name]:
+        d[name]['star'] = star
+        d[name]['time'] = time
+    if comment != '' and 'comment' not in d[name]:
+        d[name]['comment'] = comment
+        d[name]['time'] = time
+
+    if d[name].get('star', star) != star:
         if 'history_star' in d[name]:
             d[name]['history_star'].update({d[name]['time']: d[name]['star']})
         else:
             d[name]['history_star'] = {d[name]['time']: d[name]['star']}
-        d[name]['star'] = star
+        if star == '':
+            del d[name]['star']
+        else:
+            d[name]['star'] = star
         d[name]['time'] = time
-    if d[name]['comment'] != comment:
+    if d[name].get('comment', comment) != comment:
         if 'history_comment' in d[name]:
             d[name]['history_comment'].update({d[name]['time']: d[name]['comment']})
         else:
             d[name]['history_comment'] = {d[name]['time']: d[name]['comment']}
-        d[name]['comment'] = comment
+        if comment == '':
+            del d[name]['comment']
+        else:
+            d[name]['comment'] = comment
         d[name]['time'] = time
     return
 
 
-if __name__ == '__main__':
-    headers = dict()
-    try:
-        with open(save_path, 'r', encoding='utf-8') as json_file:
-            storage = json.load(json_file)
-    except FileNotFoundError:
-        storage = dict()
-    headers['User-Agent'] = random.choice(user_agent_list)
+headers = dict()
+try:
+    with open(save_path, 'r', encoding='utf-8') as json_file:
+        storage = json.load(json_file)
+        storage['last_update'] = time
+except FileNotFoundError:
+    storage = dict()
+    storage['last_update'] = time
+headers['User-Agent'] = random.choice(user_agent_list)
 
-    print('Begin Fetching...')
-    r = requests.get(url, headers=headers)
-    r.encoding = 'utf-8'
-    text = r.text.replace('\n', '').replace('\r', '')
-    page_num = int(page_re.findall(text)[-1])
-    print('Page 1 get.')
+print('Begin Fetching...')
+r = requests.get(url, headers=headers)
+r.encoding = 'utf-8'
+text = r.text.replace('\n', '').replace('\r', '')
+with open('tmp2.txt', 'w', encoding='utf-8') as tmp_file:
+    tmp_file.write(text)
+page_num = int(page_re.findall(text)[-1])
+print('Page 1 get.')
 
-    re_list = item_re.findall(text)
-    for re_tuple in re_list:
-        # print(re_tuple[0] + ' fetched.')
-        get_item(re_tuple, storage)
+re_list = item_re.findall(text)
+for re_tuple in re_list:
+    # print(re_tuple[0] + ' fetched.')
+    get_item(re_tuple, storage)
 
-    if page_num > 1:
-        for i in range(2, page_num + 1):
-            url = url + "?page=" + str(i)
-            headers['User-Agent'] = random.choice(user_agent_list)
+if page_num > 1:
+    for i in range(2, page_num + 1):
+        url = url + "?page=" + str(i)
+        headers['User-Agent'] = random.choice(user_agent_list)
 
-            r = requests.get(url, headers=headers)
-            r.encoding = 'utf-8'
-            text = r.text.replace('\n', '').replace('\r', '')
-            print('Page ' + str(i) + ' get.')
+        r = requests.get(url, headers=headers)
+        r.encoding = 'utf-8'
+        text = r.text.replace('\n', '').replace('\r', '')
+        print('Page ' + str(i) + ' get.')
 
-            re_list = item_re.findall(text)
-            for re_tuple in re_list:
-                # print(re_tuple[0] + ' fetched.')
-                get_item(re_tuple, storage)
+        re_list = item_re.findall(text)
+        for re_tuple in re_list:
+            # print(re_tuple[0] + ' fetched.')
+            get_item(re_tuple, storage)
 
-    with open(save_path, 'w', encoding='utf-8') as json_file:
-        print('Saving...')
-        json.dump(storage, json_file, ensure_ascii=False, indent=4)
-        print('Everything is OK.')
+with open(save_path, 'w', encoding='utf-8') as json_file:
+    print('Saving...')
+    json.dump(storage, json_file, ensure_ascii=False, indent=4)
+    print('Everything is OK.')
